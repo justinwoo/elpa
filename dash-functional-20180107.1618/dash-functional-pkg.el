8d3eb45ba1c61b27ecb9486129ff087e4e9a77d4
justinwoo <moomoowoo@gmail.com>
2019-05-23 13:16:56 +0300

    oiajew

--- dash-functional-20180107.1618/dash-functional-pkg.el
+++ dash-functional-20180107.1618/dash-functional-pkg.el
@@ -1,2 +1,2 @@
 ;;; -*- no-byte-compile: t -*-
-(define-package "dash-functional" "20180107.1618" "Collection of useful combinators for Emacs Lisp" '((dash "2.0.0") (emacs "24")) :commit "dca7bdcf7919dd3e589e0bb2fa210fcba34ec69a" :keywords '("lisp" "functions" "combinators"))
+(define-package "dash-functional" "20180107.1618" "Collection of useful combinators for Emacs Lisp" '((dash "2.0.0") (emacs "24")) :commit "77f3bf40c9c85386a50f2dab3dc950513f6f88bd" :keywords '("lisp" "functions" "combinators"))
