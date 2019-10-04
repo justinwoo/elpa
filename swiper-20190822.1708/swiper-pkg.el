0aa5606acefba9e54e864477f7b87aa41286fdde
justinwoo <moomoowoo@gmail.com>
2019-10-04 14:38:07 +0300

    updates

---
+++ swiper-20190822.1708/swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "swiper" "20190822.1708" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.12.0")) :commit "09507fc5605337b99108f1a439c7db07e538c3a2" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
